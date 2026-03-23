77ef0bcfa722e9b28f2e1d4c00bda11f050f0ff7
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-03-23 07:42:26 +0100

    Doc

--- pre_process.sh
+++ pre_process.sh
@@ -2,4 +2,9 @@
 #
 # From the m3inference doc
 #
+#
+# Creates files:
+# - test/data_resized.jsonl
+# - test/pic_resized/
+
 python scripts/preprocess.py --source_dir test/pic/ --output_dir test/pic_resized/ --jsonl_path test/data.jsonl --jsonl_outpath test/data_resized.jsonl --verbose
