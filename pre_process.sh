5313b45c4316779a9c6a1fe4ee232e18d9aee5c1
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-03-23 07:41:54 +0100

    Do preprocess

--- pre_process.sh
+++ pre_process.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+#
+# From the m3inference doc
+#
 python scripts/preprocess.py --source_dir test/pic/ --output_dir test/pic_resized/ --jsonl_path test/data.jsonl --jsonl_outpath test/data_resized.jsonl --verbose
