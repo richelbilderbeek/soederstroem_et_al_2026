898c72a5ef93dbfc8f16020f4fa3308dfdad63a8
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-03-26 18:06:01 +0100

    Works

--- 1_download_images.py
+++ 1_download_images.py
@@ -2,6 +2,7 @@
 import requests
 import urllib.request
 import urllib.parse
+import os
 
 bearer_token = open('bearer_token', 'r').read().strip()
 
@@ -13,9 +14,15 @@ with open(usernames_file, 'r') as f:
 
 for username in usernames:
     print("username: ", username)
-    url = f'https://api.x.com/2/users/by?usernames={urllib.parse.quote(username)}&user.fields=profile_image_url'
-    headers = {'Authorization': f'Bearer {bearer_token}'}
-    resp = requests.get(url, headers=headers).json()
-    if 'data' in resp and resp['data']:
-        img_url = resp['data'][0]['profile_image_url']
-        urllib.request.urlretrieve(img_url, f'{username}.jpg')
+    jpg_file_name = f'data/{username}.jpg'
+    print("jpg_file_name: ", jpg_file_name)
+    if not os.path.exists(jpg_file_name):
+        print("file does not exist, creating it ...")
+        url = f'https://api.x.com/2/users/by?usernames={urllib.parse.quote(username)}&user.fields=profile_image_url'
+        headers = {'Authorization': f'Bearer {bearer_token}'}
+        resp = requests.get(url, headers=headers).json()
+        if 'data' in resp and resp['data']:
+            img_url = resp['data'][0]['profile_image_url']
+            urllib.request.urlretrieve(img_url, jpg_file_name)
+    else:
+        print("file does already exist, skipping it ...")
