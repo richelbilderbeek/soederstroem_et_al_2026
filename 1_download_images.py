c4026ea4a80dfb446ca634b379c357040f63ff7b
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-03-27 18:03:26 +0100

    Create empty images

--- 1_download_images.py
+++ 1_download_images.py
@@ -3,6 +3,8 @@ import requests
 import urllib.request
 import urllib.parse
 import os
+import random
+import shutil
 
 bearer_token = open('bearer_token', 'r').read().strip()
 
@@ -14,17 +16,27 @@ usernames_file = 'missing_profile_pictures_stop_the_steal.txt'
 with open(usernames_file, 'r') as f:
     usernames = [line.strip() for line in f if line.strip()]
 
+random.shuffle(usernames) 
+
 for username in usernames:
-    # print("username: ", username)
+    #print("username: ", username)
     jpg_file_name = f'data/{username}.jpg'
-    # print("jpg_file_name: ", jpg_file_name)
+    #print("jpg_file_name: ", jpg_file_name)
     if not os.path.exists(jpg_file_name):
-        print("file does not exist, creating it ...")
         url = f'https://api.x.com/2/users/by?usernames={urllib.parse.quote(username)}&user.fields=profile_image_url'
         headers = {'Authorization': f'Bearer {bearer_token}'}
         resp = requests.get(url, headers=headers).json()
         if 'data' in resp and resp['data']:
             img_url = resp['data'][0]['profile_image_url']
             urllib.request.urlretrieve(img_url, jpg_file_name)
+            if not os.path.exists(jpg_file_name):
+                open(jpg_file_name, 'a').close()
+                print(f"Created an empty file for {username}")
+                if not os.path.exists(jpg_file_name):
+                    print("Huh? I created an empty file!")
+            else:
+                print(f"Download a profile picture from {username}")
     else:
-        print("file does already exist, skipping it ...")
+        print(f"file {username} does already exist, skipping it ...")
+
+
