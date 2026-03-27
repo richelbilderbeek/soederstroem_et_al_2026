4db572b1d0cbf08936c08a3756056b2067483ecb
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-03-27 15:52:08 +0100

    Add pics

--- 1_download_images.py
+++ 1_download_images.py
@@ -15,9 +15,9 @@ with open(usernames_file, 'r') as f:
     usernames = [line.strip() for line in f if line.strip()]
 
 for username in usernames:
-    print("username: ", username)
+    # print("username: ", username)
     jpg_file_name = f'data/{username}.jpg'
-    print("jpg_file_name: ", jpg_file_name)
+    # print("jpg_file_name: ", jpg_file_name)
     if not os.path.exists(jpg_file_name):
         print("file does not exist, creating it ...")
         url = f'https://api.x.com/2/users/by?usernames={urllib.parse.quote(username)}&user.fields=profile_image_url'
