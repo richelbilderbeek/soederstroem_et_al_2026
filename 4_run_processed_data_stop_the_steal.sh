8644119f355d645cc05790ece9f49747aaea0c3d
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-03-30 07:44:15 +0200

    Can create JSONLs

---
+++ 4_run_processed_data_stop_the_steal.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python3 4_run_processed_data_stop_the_steal.py > results_stop_the_steal.jsonl
+
---
+++ 4_run_processed_data_yttrandefrihet.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python3 4_run_processed_data_yttrandefrihet.py > results_yttrandefrihet.jsonl
+
