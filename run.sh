bf32fbdb7a408c245d455fc2e20b66024915eabf
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-04-01 14:29:38 +0200

    New StS data

--- run.sh
+++ run.sh
@@ -7,8 +7,11 @@
 ./2_pre_process.sh 
 ./3_only_keep_existing_images.R
 ./4_run_processed_data_stop_the_steal.sh
+./4_run_processed_data_stop_the_steal_text_based.sh
 ./4_run_processed_data_swexit.sh
+./4_run_processed_data_swexit_text_based.sh
 ./4_run_processed_data_yttrandefrihet.sh
+./4_run_processed_data_yttrandefrihet_text_based.sh
 ./5_jsons_to_csv.R
 echo " "
 echo "DONE!"
